7b9853599eb4303c412825ba7293ddb00f7ea724
Peter Downing <petedowning14@gmail.com>
2018-10-25 11:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function iterativeLog(array) {
     console.log(`${index}: ${element}`);
   })
 }
+
+function iterate(callback) {
+  var array = [`Cello`, `Violin`]
+  array.forEach(callback)
+  return array
+}
